b37d8c58e59d42b7a5dcf8a8b7f23a9077d10793
vijays-hub <vijayskumar82@gmail.com>
2024-12-31 13:41:19 +0530

    Feature: API Integration - Pre-filling mock documents data to storage if doesn't exist!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+
+// TODO: Fix the custom path import in TS Config
 import DocumentListWrapper from "./components/Documents";
+import {
+  getDocumentsFromStorage,
+  setDocumentsToStorage,
+} from "./lib/utils/storage";
+import MOCK_DOCUMENT_DATA from "./lib/constants";
 
 function App() {
+  useEffect(() => {
+    /**
+     * We should pre-fill our local storage with some mock data, if it's empty.
+     * This will help in having some data to work with, even if the server is down.
+     */
+    const persistedDocuments = getDocumentsFromStorage();
+    if (!persistedDocuments.length) setDocumentsToStorage(MOCK_DOCUMENT_DATA);
+  }, []);
+
   return (
     <div className="App">
       <DocumentListWrapper />
