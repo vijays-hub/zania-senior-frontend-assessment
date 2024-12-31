864c822be251694e100b5dcb7aeb83414154358b
vijays-hub <vijayskumar82@gmail.com>
2024-12-31 13:44:06 +0530

    Feature: API Integration - Using existing storage documents inside the GET API.

--- src/lib/mocks/handlers.ts
+++ src/lib/mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from "msw";
-import MOCK_DOCUMENT_DATA from "../constants";
+import { getDocumentsFromStorage } from "../utils/storage";
 
 export const handlers = [
   //   Mock a GET /documents request handler
@@ -9,21 +9,16 @@ export const handlers = [
     // TODO: Fix the type of req, res, ctx
     (req, res, ctx) => {
       /**
-       * We will fallback to the mock data if we don't have any documents in the database.
-       * Since this is FE focused, our database would be nothing but the local storage.
-       *
+       * ! Mocking the response from the server
        * !! Using local-storage for simplicity. We cn try indexedDB if the data grows.
+       *
+       * The persisted documents are fetched from the local storage. It is guaranteed that there
+       * will always be data in the local storage, as we are setting the initial data in the storage
+       * when the app is loaded for the first time. Checkout App.tsx for more info.
        */
-      const persistedDocuments = localStorage.getItem("documents")
-        ? JSON.parse(localStorage.getItem("documents") || "[]")
-        : null;
+      const persistedDocuments = getDocumentsFromStorage();
 
-      const documents =
-        persistedDocuments && persistedDocuments.length
-          ? persistedDocuments
-          : MOCK_DOCUMENT_DATA;
-
-      return HttpResponse.json(documents);
+      return HttpResponse.json(persistedDocuments);
     }
   ),
 ];
